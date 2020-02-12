89c34bcd841df22471c329900ed93fa825761103
Maina Nick <nick.maina1@gmail.com>
2020-02-12 14:57:16 +0300

    clean

--- lib/auth.ts
+++
@@ -1,8 +0,0 @@
-import { ClientResource } from './types';
-
-export default class Auth implements ClientResource {
-  readonly client: ClientResource['client'];
-  constructor(client: ClientResource['client']) {
-    this.client = client;
-  }
-}
